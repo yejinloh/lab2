d06bd8bc4549f812f69aa3337f420733bebf1f18
yejinloh <48663954+yejinloh@users.noreply.github.com>
2020-03-15 21:39:34 +0800

    update

--- lib/registerscreen.dart
+++ lib/registerscreen.dart
@@ -13,7 +13,8 @@ class RegisterScreen extends StatefulWidget {
 class _RegisterScreenState extends State<RegisterScreen> {
   bool _obscureText = true;
   bool _isChecked = false;
-  String urlRegister ="http://yjjmappflutter.com/WatchYou/php/register_user.php";
+  String urlRegister =
+      "http://yjjmappflutter.com/WatchYou/php/register_user.php";
   TextEditingController _name = new TextEditingController();
   TextEditingController _email = new TextEditingController();
   TextEditingController _phone = new TextEditingController();
@@ -39,10 +40,9 @@ class _RegisterScreenState extends State<RegisterScreen> {
                     height: 100.0, width: 200.0),
               ),
             ),
-
             TextField(
-              style: TextStyle(color: Colors.white,
-                fontFamily: 'Do Hyeon', fontSize: 20),
+              style: TextStyle(
+                  color: Colors.white, fontFamily: 'Do Hyeon', fontSize: 20),
               controller: _name,
               decoration: InputDecoration(
                 hintText: "Username",
@@ -58,10 +58,9 @@ class _RegisterScreenState extends State<RegisterScreen> {
                 ),
               ),
             ),
-
             TextField(
-              style: TextStyle(color: Colors.white,
-                fontFamily: 'Do Hyeon', fontSize: 20),
+              style: TextStyle(
+                  color: Colors.white, fontFamily: 'Do Hyeon', fontSize: 20),
               controller: _email,
               decoration: InputDecoration(
                 hintText: "Email (Ex: example12@gmail.com)",
@@ -77,10 +76,9 @@ class _RegisterScreenState extends State<RegisterScreen> {
                 ),
               ),
             ),
-
             TextField(
-              style: TextStyle(color: Colors.white,
-                fontFamily: 'Do Hyeon', fontSize: 20),
+              style: TextStyle(
+                  color: Colors.white, fontFamily: 'Do Hyeon', fontSize: 20),
               controller: _phone,
               decoration: InputDecoration(
                 hintText: "Phone (Ex: 0129876543)",
@@ -96,10 +94,9 @@ class _RegisterScreenState extends State<RegisterScreen> {
                 ),
               ),
             ),
-
             TextField(
-              style: TextStyle(color: Colors.white,
-              fontFamily: 'Do Hyeon', fontSize: 20),
+              style: TextStyle(
+                  color: Colors.white, fontFamily: 'Do Hyeon', fontSize: 20),
               controller: _password,
               decoration: InputDecoration(
                 hintText: "Password",
@@ -119,7 +116,6 @@ class _RegisterScreenState extends State<RegisterScreen> {
             new FlatButton(
                 onPressed: _toggle,
                 child: new Text(_obscureText ? "Show" : "Hide")),
-
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
@@ -236,7 +232,6 @@ class _RegisterScreenState extends State<RegisterScreen> {
             new FlatButton(
               child: new Text("Yes"),
               onPressed: _onRegister,
-              
             ),
             new FlatButton(
               child: new Text("No"),
@@ -316,8 +311,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                               fontSize: 12.0,
                             ),
                             text:
-                                "This End-User License Agreement is a legal agreement between you and Slumberjer This EULA agreement governs your acquisition and use of our MY.GROCERY software (Software) directly from Slumberjer or indirectly through a Slumberjer authorized reseller or distributor (a Reseller).Please read this EULA agreement carefully before completing the installation process and using the MY.GROCERY software. It provides a license to use the MY.GROCERY software and contains warranty information and liability disclaimers. If you register for a free trial of the MY.GROCERY software, this EULA agreement will also govern that trial. By clicking accept or installing and/or using the MY.GROCERY software, you are confirming your acceptance of the Software and agreeing to become bound by the terms of this EULA agreement. If you are entering into this EULA agreement on behalf of a company or other legal entity, you represent that you have the authority to bind such entity and its affiliates to these terms and conditions. If you do not have such authority or if you do not agree with the terms and conditions of this EULA agreement, do not install or use the Software, and you must not accept this EULA agreement.This EULA agreement shall apply only to the Software supplied by Slumberjer herewith regardless of whether other software is referred to or described herein. The terms also apply to any Slumberjer updates, supplements, Internet-based services, and support services for the Software, unless other terms accompany those items on delivery. If so, those terms apply. This EULA was created by EULA Template for MY.GROCERY. Slumberjer shall at all times retain ownership of the Software as originally downloaded by you and all subsequent downloads of the Software by you. The Software (and the copyright, and other intellectual property rights of whatever nature in the Software, including any modifications made thereto) are and shall remain the property of Slumberjer. Slumberjer reserves the right to grant licences to use the Software to third parties"
-                            )),
+                                "This End-User License Agreement is a legal agreement between you and Slumberjer This EULA agreement governs your acquisition and use of our MY.GROCERY software (Software) directly from Slumberjer or indirectly through a Slumberjer authorized reseller or distributor (a Reseller).Please read this EULA agreement carefully before completing the installation process and using the MY.GROCERY software. It provides a license to use the MY.GROCERY software and contains warranty information and liability disclaimers. If you register for a free trial of the MY.GROCERY software, this EULA agreement will also govern that trial. By clicking accept or installing and/or using the MY.GROCERY software, you are confirming your acceptance of the Software and agreeing to become bound by the terms of this EULA agreement. If you are entering into this EULA agreement on behalf of a company or other legal entity, you represent that you have the authority to bind such entity and its affiliates to these terms and conditions. If you do not have such authority or if you do not agree with the terms and conditions of this EULA agreement, do not install or use the Software, and you must not accept this EULA agreement.This EULA agreement shall apply only to the Software supplied by Slumberjer herewith regardless of whether other software is referred to or described herein. The terms also apply to any Slumberjer updates, supplements, Internet-based services, and support services for the Software, unless other terms accompany those items on delivery. If so, those terms apply. This EULA was created by EULA Template for MY.GROCERY. Slumberjer shall at all times retain ownership of the Software as originally downloaded by you and all subsequent downloads of the Software by you. The Software (and the copyright, and other intellectual property rights of whatever nature in the Software, including any modifications made thereto) are and shall remain the property of Slumberjer. Slumberjer reserves the right to grant licences to use the Software to third parties")),
                   ),
                 )
               ],
