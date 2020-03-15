692304bf2518fe262d16e6afacd4ba6463ba61a9
yejinloh <48663954+yejinloh@users.noreply.github.com>
2020-03-16 00:14:22 +0800

    update

--- lib/loginscreen.dart
+++ lib/loginscreen.dart
@@ -4,16 +4,16 @@ import 'package:lab2/registerscreen.dart';
 //import 'package:http/http.dart' as http;
 //import 'package:toast/toast.dart';
 //import 'package:shared_preferences/shared_preferences.dart';
-
+ 
 void main() => runApp(LoginScreen());
 bool rememberMe = false;
-
+ 
 class LoginScreen extends StatefulWidget {
+  
   @override
   _LoginScreenState createState() => _LoginScreenState();
 }
-
-class _LoginScreenState extends State<LoginScreen> {
+class _LoginScreenState extends State<LoginScreen>{
   bool _obscureText = true;
   TextEditingController _email = new TextEditingController();
   TextEditingController _password = new TextEditingController();
@@ -21,7 +21,7 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomPadding: false,
+      resizeToAvoidBottomPadding: false, 
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
@@ -34,87 +34,114 @@ class _LoginScreenState extends State<LoginScreen> {
             Container(
               margin: EdgeInsets.only(top: 50),
               child: Center(
-                child: Image.asset('assets/images/logo.png',
-                    height: 100.0, width: 200.0),
-              ),
+              child: Image.asset('assets/images/logo.png',
+            height: 100.0,
+            width: 200.0
             ),
+            ),
+            ),
+
             Text('\n'),
+
             TextField(
-              style: TextStyle(fontFamily: 'Do Hyeon', fontSize: 20),
+              style: TextStyle(
+                 fontFamily: 'Do Hyeon',
+                 fontSize: 20
+              ),
               controller: _email,
               decoration: InputDecoration(
-                hintText: "Email (Ex: example12@gmail.com)",
-                hintStyle: TextStyle(fontSize: 20.0, color: Colors.black38),
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(
+                 hintText: "Email (Ex: example12@gmail.com)",
+                 hintStyle: TextStyle(
+                     fontSize: 20.0, 
+                     color: Colors.black38),
+                 border: OutlineInputBorder(
+                 borderSide: BorderSide(
                     color: Colors.teal,
-                  ),
+                 ),
                 ),
-                prefixIcon: const Icon(
-                  Icons.email,
-                  color: Colors.white,
-                ),
+                 prefixIcon: const Icon(
+                   Icons.email,
+                   color: Colors.white,
+                 ),
               ),
             ),
+
             TextField(
-              style: TextStyle(fontFamily: 'Do Hyeon', fontSize: 20),
+              style: TextStyle(
+                 fontFamily: 'Do Hyeon',
+                 fontSize: 20
+              ),
               controller: _password,
               decoration: InputDecoration(
-                hintText: "Password",
-                hintStyle: TextStyle(fontSize: 20.0, color: Colors.black38),
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(
+                 hintText: "Password",
+                 hintStyle: TextStyle(
+                     fontSize: 20.0, 
+                     color: Colors.black38),
+                 border: OutlineInputBorder(
+                 borderSide: BorderSide(
                     color: Colors.teal,
-                  ),
+                 ),
                 ),
-                prefixIcon: const Icon(
-                  Icons.security,
-                  color: Colors.white,
-                ),
+                 prefixIcon: const Icon(
+                   Icons.security,
+                   color: Colors.white,
+                 ),
               ),
               obscureText: _obscureText,
             ),
             new FlatButton(
                 onPressed: _toggle,
                 child: new Text(_obscureText ? "Show" : "Hide")),
-            Row(
-              children: <Widget>[
-                Checkbox(
-                  value: rememberMe,
-                  onChanged: (bool value) {
-                    _onRememberMeChanged(value);
-                  },
-                ),
-                Text('Remember Me ',
-                    style:
-                        TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-              ],
-            ),
-            roundedRectButton("Login", loginGradients, false),
-            Text("Don't have an account? ", style: TextStyle(fontSize: 16.0)),
-            GestureDetector(
-              onTap: _registerUser,
-              child: roundedRectButton("Register", registerGradients, false),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  void _onRememberMeChanged(bool newValue) => setState(() {
-        rememberMe = newValue;
-        print(rememberMe);
-      });
-
-  void _registerUser() {
-    Navigator.push(context,
-        MaterialPageRoute(builder: (BuildContext context) => RegisterScreen()));
-  }
-
-  void _toggle() {
-    setState(() {
-      _obscureText = !_obscureText;
+                
+                            Row(
+                              children: <Widget>[
+                                  Checkbox(
+                                    value: rememberMe,
+                                    onChanged: (bool value) {
+                                      _onRememberMeChanged(value);
+                                    },),
+                                
+                                    Text('Remember Me ',
+                                     style: TextStyle(
+                                     fontSize: 16, 
+                                     fontWeight: FontWeight.bold)
+                                    ),
+                                    
+                              ],
+                            ),
+                
+                            roundedRectButton("Login", loginGradients, false),
+                
+                            Text("Don't have an account? ", style: TextStyle(fontSize: 16.0)),
+                            GestureDetector(
+                              onTap: _registerUser,
+                              child: roundedRectButton(
+                                "Register", registerGradients, false),
+                            ),
+                
+                                      
+                                
+                                
+                                          ],
+                                        ),
+                                      ),
+                                    );
+                                  }
+                                
+                                  void _onRememberMeChanged(bool newValue) => setState(() {
+                                        rememberMe = newValue;
+                                        print(rememberMe);
+                                      });
+                                  
+                                
+                                  void _registerUser() {
+                                    Navigator.push(context,
+                                    MaterialPageRoute(builder: (BuildContext context) => RegisterScreen()));
+                  }
+                
+                  void _toggle() {
+                    setState(() {
+                   _obscureText = !_obscureText;
     });
   }
 }
